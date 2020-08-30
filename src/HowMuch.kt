e9e124303d9b60cfa606e89073835b09ee394c32
mikeni <maac.1106@gmail.com>
2020-08-30 11:59:50 -0500

    improvement how much

--- src/HowMuch.kt
+++ src/HowMuch.kt
@@ -1,19 +1,10 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-fun howmuch(m: Int, n: Int): String {
-    val result = if (m < n) {
-        (m..n)
-            .filter { money -> (money - 2).rem(7) == 0 && (money - 1).rem(9) == 0 }
-            .joinToString("") { "[M: $it B: ${it / 7} C: ${it / 9}]" }
-    } else {
-        (n..m)
-            .filter { money -> (money - 2).rem(7) == 0 && (money - 1).rem(9) == 0 }
-            .joinToString("") { "[M: $it B: ${it / 7} C: ${it / 9}]" }
-    }
-
-    return "[$result]"
-}
+fun howmuch(m: Int, n: Int): String =
+    (Math.min(m, n)..Math.max(m, n))
+        .filter { money -> (money - 2).rem(7) == 0 && (money - 1).rem(9) == 0 }
+        .joinToString("", "[", "]") { "[M: $it B: ${it / 7} C: ${it / 9}]" }
 
 class CarboatTest {
     @Test
@@ -21,6 +12,5 @@ class CarboatTest {
         println("****** Basic tests ******")
         assertEquals("[[M: 37 B: 5 C: 4][M: 100 B: 14 C: 11]]", howmuch(1, 100))
         assertEquals("[[M: 37 B: 5 C: 4][M: 100 B: 14 C: 11][M: 163 B: 23 C: 18]]", howmuch(0, 200))
-        assertEquals("[[M: 9991 B: 1427 C: 1110]]", howmuch(0, 1000))
     }
 }
